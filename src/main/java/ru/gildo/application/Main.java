97c96796d042410dca4875707bda52bcb0f50a9f
tachank-a <gildo2015@yandex.ru>
2023-01-12 23:49:42 +0700

    refactoring

--- src/main/java/ru/gildo/application/Main.java
+++ src/main/java/ru/gildo/application/Main.java
@@ -1,15 +1,26 @@
 package ru.gildo.application;
 
+import ru.gildo.application.action.ActionHandler;
+import ru.gildo.application.action.ActionHandlerForC;
 import ru.gildo.application.handler.LexemeHandler;
 import ru.gildo.application.core.Core;
+
+import java.util.List;
+import java.util.Map;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Core machine = new Core(LexemeHandler.createListOfLexemes());
+        ActionHandler actionHandler = new ActionHandlerForC();
 
-        machine.startAnalyse();
+        List<Map<Integer, String>> lexemes = LexemeHandler.createListOfLexemes();
+
+        System.out.println(lexemes);
+
+        Core core = new Core(lexemes, actionHandler);
+
+        core.startAnalyse();
 
     }
 }
--- src/main/java/ru/gildo/application/core/Core.java
+++ src/main/java/ru/gildo/application/core/Core.java
@@ -1,24 +1,25 @@
 package ru.gildo.application.core;
 
-
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import ru.gildo.application.action.ActionHandler;
 import ru.gildo.application.core.exception.InvalidSymbolException;
 
 import java.util.*;
 
-
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Core {
-
+    @NonNull
     private final List<Map<Integer, String>> lexemesWithPosition;
-
+    @NonNull
+    private final ActionHandler actionHandler;
+    private int activeState = 0;
 
     public void startAnalyse() {
         try {
             int stringCount = 1;
             for (Map<Integer, String> lexemeWithPosition :
                     lexemesWithPosition) {
-                System.out.println(lexemeWithPosition);
 
                 int finalStringCount = stringCount;
                 lexemeWithPosition.forEach((position, lexeme) -> TEMPcheckValue(finalStringCount, position, lexeme.toCharArray()));
@@ -32,10 +33,11 @@ public class Core {
     public void TEMPcheckValue(int stringNumber, Integer position, char[] lexeme) {
 //        for (int i = 0; i < lexeme.length; i++) {
 //            if (lexeme[i] == 'm') {
-//                throw new InvalidSymbol(stringNumber, position, lexeme[i]);
+//                throw new InvalidSymbolException(stringNumber, position, lexeme[i]);
 //            }
 //            position++;
 //        }
+        actionHandler.actionCall(1);
     }
 
 }
